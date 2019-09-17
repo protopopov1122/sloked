e25466410a89fcb576dd44972710bd4897642af7
J. Protopopovs <jprotopopov1122@gmail.com>
2019-09-17 21:09:47 +0200

    Update license

--- src/text/TextFrame.cpp
+++ src/text/TextFrame.cpp
@@ -1,14 +1,14 @@
 /*
-  SPDX-License-Identifier: LGPL-3.0-or-later
+  SPDX-License-Identifier: LGPL-3.0
 
   Copyright (c) 2019 Jevgenijs Protopopovs
 
   This file is part of Sloked project.
 
   Sloked is free software: you can redistribute it and/or modify
-  it under the terms of the GNU Lesser General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
+  it under the terms of the GNU Lesser General Public License version 3 as published by
+  the Free Software Foundation.
+
 
   Sloked is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
