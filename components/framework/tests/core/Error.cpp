8c5e9a38f3efadf8cbc11bfbf55d5b4f66493c22
J. Protopopovs <jprotopopov1122@gmail.com>
2020-02-29 16:16:18 +0100

    Implement error unit test

---
+++ components/framework/tests/core/Error.cpp
@@ -0,0 +1,34 @@
+/*
+  SPDX-License-Identifier: LGPL-3.0
+
+  Copyright (c) 2019-2020 Jevgenijs Protopopovs
+
+  This file is part of Sloked project.
+
+  Sloked is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License version 3 as published by
+  the Free Software Foundation.
+
+
+  Sloked is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with Sloked.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "catch2/catch.hpp"
+#include "sloked/core/Error.h"
+
+using namespace sloked;
+
+TEST_CASE("Error stores error message") {
+    const std::string Message = GENERATE("hello", "world", "ERROR MSG");
+    try {
+        throw SlokedError(Message);
+    } catch (const SlokedError &ex) {
+        REQUIRE(std::string{ex.what()} == Message);
+    }
+}
