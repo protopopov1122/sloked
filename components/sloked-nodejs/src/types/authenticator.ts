2f22f643285bd32849fe6b4e7c0d8b85766b70ab
J. Protopopovs <jprotopopov1122@gmail.com>
2020-04-27 20:57:17 +0200

    Add license headers to all TS source files

--- components/sloked-nodejs/src/types/authenticator.ts
+++ components/sloked-nodejs/src/types/authenticator.ts
@@ -1,3 +1,24 @@
+/*
+  SPDX-License-Identifier: LGPL-3.0
+
+  Copyright (c) 2019-2020 Jevgenijs Protopopovs
+
+  This file is part of Sloked project.
+
+  Sloked is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License version 3 as
+  published by the Free Software Foundation.
+
+
+  Sloked is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with Sloked.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 export interface Authenticator {
     authenticate(account: string, nonce: number): Promise<string>
     close(): void
